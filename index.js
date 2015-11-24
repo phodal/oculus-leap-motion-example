15d3058005e4441242c4d722b1abfe5d32f53ae2
Fengda HUANG <h@phodal.com>
2015-11-24 21:38:59 +0800

    init projects

---
+++ index.js
@@ -0,0 +1,122 @@
+var hmd = require("node-hmd"),
+    express = require("express"),
+    http = require("http").createServer(),
+    WebSocketServer = require('ws').Server,
+    path = require('path');
+
+var leapjs = require('leapjs');
+
+var controller = new leapjs.Controller();
+
+
+// Create HMD manager object
+console.info("Attempting to load node-hmd driver: oculusrift");
+var manager = hmd.createManager("oculusrift");
+if (typeof(manager) === "undefined") {
+    console.error("Unable to load driver: oculusrift");
+    process.exit(1);
+}
+// Instantiate express server
+var app = express();
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(path.join(__dirname + '/', 'public')));
+app.set('views', path.join(__dirname + '/public/', 'views'));
+app.set('view engine', 'jade');
+
+app.get('/demo', function (req, res) {
+    'use strict';
+    res.render('demo', {
+        title: 'Home'
+    });
+});
+
+
+app.get("/", function (req, res) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    res.json({quat: manager.getDeviceQuatSync(), position: manager.getDevicePositionSync()})
+});
+
+app.get("/supported", function (req, res) {
+    res.json(hmd.getSupportedDevices());
+});
+
+app.get("/info", function (req, res) {
+    manager.getDeviceInfo(function (err, deviceInfo) {
+        res.json(deviceInfo);
+    });
+});
+
+app.get("/orientation", function (req, res) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    manager.getDeviceOrientation(function (err, deviceOrientation) {
+        res.json(deviceOrientation);
+    });
+});
+
+
+controller.on('connect', function(data) {
+    console.log("Successfully connected.");
+});
+
+controller.on('streamingStarted', function(data) {
+    console.log("A Leap device has been connected.");
+});
+
+controller.on('streamingStopped', function(data) {
+    console.log("A Leap device has been disconnected.");
+});
+
+controller.connect();
+
+// Attach socket.io listener to the server
+var wss = new WebSocketServer({server: http});
+var id = 1;
+
+wss.on('open', function open() {
+    console.log('connected');
+});
+
+// On socket connection set up event emitters to automatically push the HMD orientation data
+wss.on("connection", function (ws) {
+    function emitOrientation() {
+        id = id + 1;
+        var deviceQuat = manager.getDeviceQuatSync();
+        var devicePosition = manager.getDevicePositionSync();
+
+        var data = JSON.stringify({
+            id: id,
+            quat: deviceQuat,
+            position: devicePosition
+        });
+
+        ws.send(data, function (error) {
+            //it's a bug of websocket, see in https://github.com/websockets/ws/issues/337
+        });
+    }
+
+    var orientation = setInterval(emitOrientation, 1000);
+
+    ws.on("message", function (data) {
+        clearInterval(orientation);
+        orientation = setInterval(emitOrientation, data);
+    });
+
+    ws.on("close", function () {
+        setTimeout(null, 500);
+        clearInterval(orientation);
+        console.log("disconnect");
+    });
+});
+
+// Launch express server
+http.on('request', app);
+http.listen(3000, function () {
+    console.log("Express server listening on port 3000");
+});
---
+++ public/js/app.js
@@ -0,0 +1,267 @@
+if (!Detector.webgl) {
+    Detector.addGetWebGLMessage();
+    document.getElementById('container').innerHTML = "";
+}
+var container, stats, effect, oculusControl;
+var camera, scene, renderer;
+var mesh;
+var worldWidth = 128, worldDepth = 128,
+    worldHalfWidth = worldWidth / 2, worldHalfDepth = worldDepth / 2,
+    data = generateHeight(worldWidth, worldDepth);
+var clock = new THREE.Clock();
+
+init();
+animate();
+
+var colors = [0xff0000, 0x00ff00, 0x0000ff];
+var baseBoneRotation = (new THREE.Quaternion).setFromEuler(
+    new THREE.Euler(Math.PI / 2, 0, 0)
+);
+
+Leap.loop({background: true}, {
+        hand: function (hand) {
+            hand.fingers.forEach(function (finger) {
+                // This is the meat of the example - Positioning `the cylinders on every frame:
+                finger.data('boneMeshes').forEach(function (mesh, i) {
+                    var bone = finger.bones[i];
+                    mesh.position.fromArray(bone.center());
+                    mesh.setRotationFromMatrix(
+                        (new THREE.Matrix4).fromArray(bone.matrix())
+                    );
+                    mesh.quaternion.multiply(baseBoneRotation);
+                });
+                finger.data('jointMeshes').forEach(function (mesh, i) {
+                    var bone = finger.bones[i];
+                    if (bone) {
+                        mesh.position.fromArray(bone.prevJoint);
+                    } else {
+                        // special case for the finger tip joint sphere:
+                        bone = finger.bones[i - 1];
+                        mesh.position.fromArray(bone.nextJoint);
+                    }
+                });
+            });
+            var armMesh = hand.data('armMesh');
+            armMesh.position.fromArray(hand.arm.center());
+            armMesh.setRotationFromMatrix(
+                (new THREE.Matrix4).fromArray(hand.arm.matrix())
+            );
+            armMesh.quaternion.multiply(baseBoneRotation);
+            armMesh.scale.x = hand.arm.width / 2;
+            armMesh.scale.z = hand.arm.width / 4;
+            renderer.render(scene, camera);
+        }
+    })
+    // these two LeapJS plugins, handHold and handEntry are available from leapjs-plugins, included above.
+    // handHold provides hand.data
+    // handEntry provides handFound/handLost events.
+    .use('handHold')
+    .use('handEntry')
+    .on('handFound', function (hand) {
+        hand.fingers.forEach(function (finger) {
+            var boneMeshes = [];
+            var jointMeshes = [];
+            finger.bones.forEach(function (bone) {
+                // create joints
+                // CylinderGeometry(radiusTop, radiusBottom, height, radiusSegments, heightSegments, openEnded)
+                var boneMesh = new THREE.Mesh(
+                    new THREE.CylinderGeometry(5, 5, bone.length),
+                    new THREE.MeshPhongMaterial()
+                );
+                boneMesh.material.color.setHex(0xffffff);
+                scene.add(boneMesh);
+                boneMeshes.push(boneMesh);
+            });
+            for (var i = 0; i < finger.bones.length + 1; i++) {
+                var jointMesh = new THREE.Mesh(
+                    new THREE.SphereGeometry(8),
+                    new THREE.MeshPhongMaterial()
+                );
+                jointMesh.material.color.setHex(0x0088ce);
+                scene.add(jointMesh);
+                jointMeshes.push(jointMesh);
+            }
+            finger.data('boneMeshes', boneMeshes);
+            finger.data('jointMeshes', jointMeshes);
+        });
+        if (hand.arm) { // 2.0.3+ have arm api,
+            // CylinderGeometry(radiusTop, radiusBottom, height, radiusSegments, heightSegments, openEnded)
+            var armMesh = new THREE.Mesh(
+                new THREE.CylinderGeometry(1, 1, hand.arm.length, 64),
+                new THREE.MeshPhongMaterial()
+            );
+            armMesh.material.color.setHex(0xffffff);
+            scene.add(armMesh);
+            hand.data('armMesh', armMesh);
+        }
+    })
+    .on('handLost', function (hand) {
+        hand.fingers.forEach(function (finger) {
+            var boneMeshes = finger.data('boneMeshes');
+            var jointMeshes = finger.data('jointMeshes');
+            boneMeshes.forEach(function (mesh) {
+                scene.remove(mesh);
+            });
+            jointMeshes.forEach(function (mesh) {
+                scene.remove(mesh);
+            });
+            finger.data({
+                boneMeshes: null,
+                boneMeshes: null
+            });
+        });
+        var armMesh = hand.data('armMesh');
+        scene.remove(armMesh);
+        hand.data('armMesh', null);
+        renderer.render(scene, camera);
+    })
+    .connect();
+
+function init() {
+    container = document.getElementById('container');
+
+    camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 20000);
+    camera.position.y = getY(worldHalfWidth, worldHalfDepth) * 100 + 100;
+
+    oculusControl = new THREE.DK2Controls(camera);
+
+    scene = new THREE.Scene();
+    // sides
+    var matrix = new THREE.Matrix4();
+    var pxGeometry = new THREE.PlaneBufferGeometry(100, 100);
+    pxGeometry.attributes.uv.array[1] = 0.5;
+    pxGeometry.attributes.uv.array[3] = 0.5;
+    pxGeometry.rotateY(Math.PI / 2);
+    pxGeometry.translate(50, 0, 0);
+
+    var nxGeometry = new THREE.PlaneBufferGeometry(100, 100);
+    nxGeometry.attributes.uv.array[1] = 0.5;
+    nxGeometry.attributes.uv.array[3] = 0.5;
+    nxGeometry.rotateY(-Math.PI / 2);
+    nxGeometry.translate(-50, 0, 0);
+
+    var pyGeometry = new THREE.PlaneBufferGeometry(100, 100);
+    pyGeometry.attributes.uv.array[5] = 0.5;
+    pyGeometry.attributes.uv.array[7] = 0.5;
+    pyGeometry.rotateX(-Math.PI / 2);
+    pyGeometry.translate(0, 50, 0);
+
+    var pzGeometry = new THREE.PlaneBufferGeometry(100, 100);
+    pzGeometry.attributes.uv.array[1] = 0.5;
+    pzGeometry.attributes.uv.array[3] = 0.5;
+    pzGeometry.translate(0, 0, 50);
+
+    var nzGeometry = new THREE.PlaneBufferGeometry(100, 100);
+    nzGeometry.attributes.uv.array[1] = 0.5;
+    nzGeometry.attributes.uv.array[3] = 0.5;
+    nzGeometry.rotateY(Math.PI);
+    nzGeometry.translate(0, 0, -50);
+    //
+    // BufferGeometry cannot be merged yet.
+    var tmpGeometry = new THREE.Geometry();
+    var pxTmpGeometry = new THREE.Geometry().fromBufferGeometry(pxGeometry);
+    var nxTmpGeometry = new THREE.Geometry().fromBufferGeometry(nxGeometry);
+    var pyTmpGeometry = new THREE.Geometry().fromBufferGeometry(pyGeometry);
+    var pzTmpGeometry = new THREE.Geometry().fromBufferGeometry(pzGeometry);
+    var nzTmpGeometry = new THREE.Geometry().fromBufferGeometry(nzGeometry);
+    for (var z = 0; z < worldDepth; z++) {
+        for (var x = 0; x < worldWidth; x++) {
+            var h = getY(x, z);
+            matrix.makeTranslation(
+                x * 100 - worldHalfWidth * 100,
+                h * 100,
+                z * 100 - worldHalfDepth * 100
+            );
+            var px = getY(x + 1, z);
+            var nx = getY(x - 1, z);
+            var pz = getY(x, z + 1);
+            var nz = getY(x, z - 1);
+            tmpGeometry.merge(pyTmpGeometry, matrix);
+            if (( px !== h && px !== h + 1 ) || x === 0) {
+                tmpGeometry.merge(pxTmpGeometry, matrix);
+            }
+            if (( nx !== h && nx !== h + 1 ) || x === worldWidth - 1) {
+                tmpGeometry.merge(nxTmpGeometry, matrix);
+            }
+            if (( pz !== h && pz !== h + 1 ) || z === worldDepth - 1) {
+                tmpGeometry.merge(pzTmpGeometry, matrix);
+            }
+            if (( nz !== h && nz !== h + 1 ) || z === 0) {
+                tmpGeometry.merge(nzTmpGeometry, matrix);
+            }
+        }
+    }
+
+    var geometry = new THREE.BufferGeometry().fromGeometry(tmpGeometry);
+    geometry.computeBoundingSphere();
+
+    var texture = THREE.ImageUtils.loadTexture('./js/textures/minecraft/atlas.png');
+    texture.magFilter = THREE.NearestFilter;
+    texture.minFilter = THREE.LinearMipMapLinearFilter;
+
+    var mesh = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial({map: texture}));
+    scene.add(mesh);
+    var ambientLight = new THREE.AmbientLight(0xcccccc);
+    scene.add(ambientLight);
+    var directionalLight = new THREE.DirectionalLight(0xffffff, 2);
+    directionalLight.position.set(1, 1, 0.5).normalize();
+    scene.add(directionalLight);
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setClearColor(0xbfd1e5);
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    //effect = new THREE.OculusRiftEffect(renderer, { worldScale: 100 });
+    //effect.setSize(window.innerWidth, window.innerHeight);
+
+    container.innerHTML = "";
+    container.appendChild(renderer.domElement);
+    stats = new Stats();
+    stats.domElement.style.position = 'absolute';
+    stats.domElement.style.top = '0px';
+    container.appendChild(stats.domElement);
+    //
+    window.addEventListener('resize', onWindowResize, false);
+
+}
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    //effect.setSize(window.innerWidth, window.innerHeight);
+}
+
+function generateHeight(width, height) {
+    var data = [], perlin = new ImprovedNoise(),
+        size = width * height, quality = 2, z = Math.random() * 100;
+    for (var j = 0; j < 4; j++) {
+        if (j === 0) for (var i = 0; i < size; i++) data[i] = 0;
+        for (var i = 0; i < size; i++) {
+            var x = i % width, y = ( i / width ) | 0;
+            data[i] += perlin.noise(x / quality, y / quality, z) * quality;
+        }
+        quality *= 4;
+    }
+    return data;
+}
+function getY(x, z) {
+    return ( data[x + z * worldWidth] * 0.2 ) | 0;
+}
+//
+function animate() {
+    requestAnimationFrame(animate);
+    render();
+    stats.update();
+}
+function render() {
+    oculusControl.update(clock.getDelta());
+    //THREE.AnimationHandler.update( clock.getDelta() * 100 );
+
+    camera.useQuaternion = true;
+    camera.matrixWorldNeedsUpdate = true;
+
+    //effect.render(scene, camera);
+    renderer.render(scene, camera);
+}
